a40dc4fbbf92372230ec9ab9cfb8b6c0c03c09e6
HenrikAmar <henrikwaarst@gmail.com>
2026-09-01 00:12:42 +0200

    Update route.ts

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -5,7 +5,7 @@ import { createClient } from "@/lib/supabase/server";
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get("code");
-  const next = searchParams.get("next") ?? "/tip";
+  const next = searchParams.get("next") ?? "/";
   const ref = searchParams.get("ref");
 
   if (code) {
